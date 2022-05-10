9540967bdaf0c23874f4fa31a492089d96a5f3d3
ugochukwu <ugochukwuaugustine299@gmail.com>
2022-05-10 00:22:19 -0700

    using useState

--- my-new-project/App.js
+++ my-new-project/App.js
@@ -3,17 +3,15 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState ('austine')
+  const [person, setPerson] = useState({name: 'austine', age: 30,})
 
-const [name, setName] = useState ('austine')
-const [person, setPerson] = useState({name: 'austine', age: 30,})
-
-const setHandler = ()=> {
-  setName('imah ugochukwu ')
-  setPerson({name: ugochukwu, age: 40})
-}
+  const setHandler = ()=> {
+    setName('imah ugochukwu ')
+    setPerson({name: ugochukwu, age: 40})
+  }
   return (
     <View style={styles.container}>
-      
       <View style={styles.body}>
         <Text>my name is {name}</Text>
         <Text>His Name is {person.name} and his age is {person.age}</Text>
