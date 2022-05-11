6db9edaa8c0de20c6ab2801d9462dbcf9370c891
austine <46765685+austine299@users.noreply.github.com>
2022-05-11 15:57:20 -0700

    Using use state (#3)
    * using useState
    
    * using useState
    
    * using useState

--- my-new-project/App.js
+++ my-new-project/App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState ('austine')
+  const [person, setPerson] = useState({name: 'austine', age: 30,})
+
+  const setHandler = ()=> {
+    setName('imah ugochukwu ')
+    setPerson({name: ugochukwu, age: 40})
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.body}>
+        <Text>my name is {name}</Text>
+        <Text>His Name is {person.name} and his age is {person.age}</Text>
+      </View>
+      <View style = {styles.buttonContainer}>
+        <Button title='Set Name' onPress={setHandler}/>
+      </View>
+
     </View>
   );
 }
@@ -17,4 +31,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  boldText:{
+    fontWeight: 'bold',
+  },
+  body:{
+    padding:20,
+  }, 
+  buttonContainer:{
+    marginTop:20
+  }
 });
