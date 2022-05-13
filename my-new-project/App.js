7597f2a8e0f2a2c9d9ecb20caf00827128e677ea
austine <46765685+austine299@users.noreply.github.com>
2022-05-12 17:13:25 -0700

    Text input (#6)
    * using useState
    
    * using useState
    
    * using useState
    
    * making use of TextInput, using it to change text
    
    * making use of TextInput, using it to change text
    
    * making use of TextInput, using it to change text
    
    * making use of TextInput and useState, using it to change text

--- my-new-project/App.js
+++ my-new-project/App.js
@@ -1,7 +1,9 @@
 
-import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+
+
+
 
 export default function App() {
   const [name, setName] = useState ('austine')
@@ -9,18 +11,30 @@ export default function App() {
 
   const setHandler = ()=> {
     setName('imah ugochukwu ')
-
-    setPerson({name: ugochukwu, age: 40})
+    setPerson({name: 'ugochukwu', age: 40})
   }
   return (
     <View style={styles.container}>
-      <View style={styles.body}>
-        <Text>my name is {name}</Text>
-        <Text>His Name is {person.name} and his age is {person.age}</Text>
-      </View>
-      <View style = {styles.buttonContainer}>
-        <Button title='Set Name' onPress={setHandler}/>
-      </View>
+      
+      <text>Enter your name here</text>
+      <TextInput 
+        multiline
+        keyboardType='default'
+        style ={styles.input} 
+        placeholder = "e.g john mike"
+        onChangeText={(val) => setName(val)}
+      />
+
+      <text>Enter your name here</text>
+      <TextInput 
+        keyboardType='numeric'
+        style ={styles.input} 
+        placeholder = "e.g john mike"
+        onChangeText={(val) => setName(val)}
+      />
+
+      <Text>name: {name}, age: {age}</Text>
+
     </View>
   );
 }
@@ -40,6 +54,12 @@ const styles = StyleSheet.create({
   }, 
   buttonContainer:{
     marginTop:20
-
+  },
+  input:{
+    borderWidth: 1,
+    borderColor:'#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
   }
 });
